788973e25e251225f2fce4db5fcfae846e8ddb5a
Robert Golden <rgolden07@gmail.com>
2024-04-13 18:14:08 -0500

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -6,22 +6,10 @@ const Model = mongoose.model('trips');
 // regardless of outcome, response must include HTML status code
 //and JSON message to the requesting client
 const tripsList = async (req, res) => {
-    //model.find({}).then((err, trips) => {
-    //    if (!trips) {
-    //        return res.status(404).json({ "message": "trip not found" });
-    //    } else {
-    //        return res.status(200).json(trips);
-    //    }
-    //});
-
 
     const q = await Model
         .find({})
-        .exec();
-
-    // uncomment the following line to show results of querey
-    // on the console
-    //console.log();
+        .exec();    
 
     if (!q) {
         return res.status(404).json(err);
@@ -42,7 +30,63 @@ const tripsFindCode = async (req, res) => {
     }
 };
 
+// post: /trips - adds a new trip
+// regardless of outcome, response must include HTML status code
+//and JSON message to the requesting client
+const tripsAddTrip = async (req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    })
+
+    const r = await newTrip.save();
+
+        if (!r) {
+            return res.status(400).json(err);
+        } else {
+            return res.status(201).json(r);
+        }
+}
+
+//put /trips/:tripCode - adds a new trip
+// regardless of outcome, response must include HTML status code
+//and JSON message to the requesting client
+
+const tripsUpdateTrip = async (req, res) => {
+
+    console.log(req.params);
+    console.log(req.body);
+
+    const t = await Model.findOneAndUpdate(
+        { 'code': req.params.tripCode },
+        {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        }
+    ).exec();
+
+    if (!t) {
+        return res.status(400).json(err);
+    } else {
+        return res.status(201).json(t)
+    }
+}
+
 module.exports = {
     tripsList,
-    tripsFindCode
+    tripsFindCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 //import statement for the controllers
 const tripsController = require('../controllers/trips');
 
-router.route('/trips').get(tripsController.tripsList);
-router.route('/trips/:tripCode').get(tripsController.tripsFindCode);
+router.route('/trips').get(tripsController.tripsList).post(tripsController.tripsAddTrip);
+router.route('/trips/:tripCode').get(tripsController.tripsFindCode).put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
